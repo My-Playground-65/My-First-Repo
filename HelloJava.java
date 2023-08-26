fee0e572cc250e72dac84652193f71124936345e
Sahan Lakmal <sahanloser@gmail.com>
2023-08-26 20:17:56 +0530

    Updated Repo

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,7 @@
 public class HelloJava{
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("hello sahan");
+        System.out.println("hello Sahan Lakmal");
+        System.out.println("Welcome to coding platform");
     }
  }
